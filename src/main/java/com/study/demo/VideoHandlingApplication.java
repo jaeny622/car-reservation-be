00dd6b93d0821a391b8d5a2c4a39c9dc0e510902
jisu_oh <jisu_oh@tmax.co.kr>
2024-12-15 23:26:52 +0900

    [chore] initial commit

---
+++ src/main/java/com/study/demo/VideoHandlingApplication.java
@@ -0,0 +1,14 @@
+package com.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoHandlingApplication.class, args);
+		System.out.println("HELLO");
+	}
+
+}
