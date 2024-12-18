f80b16706164842d534bb91f07fc8db22af6f9fe
jisu_oh <jisu_oh@tmax.co.kr>
2024-12-18 22:42:26 +0900

    [feat] user 관련 서비스 파일 생성 및 Login, Join, 아이디 중복체크 함수 선언

---
+++ src/main/java/com/study/demo/user/UserService.java
@@ -0,0 +1,40 @@
+package com.study.demo.user;
+
+import com.study.demo.user.dto.JoinReq;
+import com.study.demo.user.dto.LoginReq;
+import com.study.demo.user.entity.Userinfo;
+import com.study.demo.user.repository.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    // 로그인 아이디 중복 체크
+    public boolean checkIdDuplication(String id){
+        return userInfoRepository.existsById(id);
+    }
+
+    // 회원가입
+    public void join(JoinReq req){
+        userInfoRepository.save(req.toEntity());
+    }
+
+    // 로그인
+    public Userinfo login(LoginReq req){
+        Userinfo user = userInfoRepository.findById(req.getId()).orElse(null);
+
+        if(user == null){
+            return null;
+        }
+
+        if(!req.getPassword().equals(user.getPassword())){
+            return null;
+        }
+
+        return user;
+    }
+}
