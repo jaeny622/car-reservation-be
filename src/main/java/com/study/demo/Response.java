7d9b3319478c434ecd03287f7b05202930e95b93
jisu_oh <jisu_oh@tmax.co.kr>
2024-12-18 22:52:31 +0900

    [chore] response record 이름 변경

---
+++ src/main/java/com/study/demo/Response.java
@@ -0,0 +1,16 @@
+package com.study.demo;
+
+import jakarta.annotation.Nullable;
+
+public record Response<T>(
+        String status,
+        @Nullable T data
+) {
+    public static <T> Response<T> success(@Nullable T data) {
+        return new Response<>("success",data);
+    }
+
+    public static <T> Response<T> created(@Nullable T data) {
+        return new Response<>("success",data);
+    }
+}
